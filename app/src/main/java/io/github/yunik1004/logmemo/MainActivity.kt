31994a8ffc928ff2dfb6556b23c9b7832a927db8
yunik1004 <inkyuhak@gmail.com>
2018-12-24 08:46:23 +0900

    Implement instantly saved edittext

--- app/src/main/java/io/github/yunik1004/logmemo/MainActivity.kt
+++ app/src/main/java/io/github/yunik1004/logmemo/MainActivity.kt
@@ -1,30 +1,55 @@
 package io.github.yunik1004.logmemo
 
+import android.content.res.ColorStateList
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
+import android.widget.EditText
 import org.jetbrains.anko.*
 import org.jetbrains.anko.design.floatingActionButton
 import org.jetbrains.anko.recyclerview.v7.recyclerView
 import org.jetbrains.anko.sdk27.coroutines.onClick
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainUI: MainActivityUi
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        MainActivityUi().setContentView(this)
+        mainUI = MainActivityUi()
+        mainUI.setContentView(this)
     }
 }
 
 class MainActivityUi : AnkoComponent<MainActivity> {
+    private lateinit var memo: EditText
+
     override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
         verticalLayout {
             padding = dip(8)
 
+            // Setting button
             floatingActionButton {
-                backgroundColor = Color.BLUE
+                imageResource = R.drawable.ic_more_vert_black_24dp
+                backgroundTintList = ColorStateList.valueOf(Color.TRANSPARENT)
+                compatElevation = 0.0f
+                onClick {
+                    toast("Settings").show()
+                }
+            }.lparams {
+                gravity = Gravity.END
+            }
+
+            // Color-change button
+            floatingActionButton {
                 imageResource = R.drawable.ic_invert_colors_black_24dp
+                backgroundTintList = ColorStateList.valueOf(Color.TRANSPARENT)
+                compatElevation = 0.0f
+                onClick {
+                    toast("Change color").show()
+                }
+            }.lparams {
+                gravity = Gravity.END
             }
 
             recyclerView {
@@ -33,7 +58,8 @@ class MainActivityUi : AnkoComponent<MainActivity> {
 
             linearLayout {
                 // Memo input
-                val name = editText {
+                memo = editText {
+                    id = R.id.memo_edit_text
                     hintResource = R.string.memo_write_hint
                     maxLines = 5
                 }.lparams(width = wrapContent, height = wrapContent, weight = 1.0f){}
@@ -41,12 +67,16 @@ class MainActivityUi : AnkoComponent<MainActivity> {
                 // Memo save button
                 button(R.string.memo_save) {
                     onClick {
-                        toast(name.text.toString()).show()
+                        toast(getMemo()).show()
                     }
                 }.lparams(width = wrapContent, height = wrapContent){
                     gravity = Gravity.BOTTOM
                 }
             }.lparams(width = matchParent, height = wrapContent){}
         }
+    }
+
+    private fun getMemo(): String {
+        return memo.text.toString()
     }
 }
