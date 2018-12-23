1d118ad02fce77dc670664257d3cc62c134a678f
yunik1004 <inkyuhak@gmail.com>
2018-12-23 15:28:52 +0900

    Layout changed

--- app/src/main/java/io/github/yunik1004/logmemo/MainActivity.kt
+++ app/src/main/java/io/github/yunik1004/logmemo/MainActivity.kt
@@ -1,31 +1,52 @@
 package io.github.yunik1004.logmemo
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
+import android.view.Gravity
 import org.jetbrains.anko.*
+import org.jetbrains.anko.design.floatingActionButton
+import org.jetbrains.anko.recyclerview.v7.recyclerView
 import org.jetbrains.anko.sdk27.coroutines.onClick
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        createView()
+        MainActivityUi().setContentView(this)
     }
+}
 
-    private fun createView() {
+class MainActivityUi : AnkoComponent<MainActivity> {
+    override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
         verticalLayout {
             padding = dip(8)
 
-            val name: EditText = editText {
-                hint = getString(R.string.memo_write_hint)
+            floatingActionButton {
+                backgroundColor = Color.BLUE
+                imageResource = R.drawable.ic_invert_colors_black_24dp
             }
 
-            button(getString(R.string.memo_save)) {
-                onClick {
-                    toast(name.text.toString()).show()
+            recyclerView {
+
+            }.lparams(width = matchParent, height = wrapContent, weight = 1.0f){}
+
+            linearLayout {
+                // Memo input
+                val name = editText {
+                    hintResource = R.string.memo_write_hint
+                    maxLines = 5
+                }.lparams(width = wrapContent, height = wrapContent, weight = 1.0f){}
+
+                // Memo save button
+                button(R.string.memo_save) {
+                    onClick {
+                        toast(name.text.toString()).show()
+                    }
+                }.lparams(width = wrapContent, height = wrapContent){
+                    gravity = Gravity.BOTTOM
                 }
-            }
+            }.lparams(width = matchParent, height = wrapContent){}
         }
     }
 }
